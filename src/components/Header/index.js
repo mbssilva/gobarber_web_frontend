b30911209fb79b400c4d170910ab4d24f977996b
mbssilva <mbernardi.s@outlook.com>
2020-03-30 20:15:59 -0300

    Estilização das notificações implementada e finalizada

---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Notifications from '../Notifications';
+
+import { Container, Content, Profile } from './styles';
+
+import logoPurple from '../../assets/logo-purple.svg';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <Link to="/dashboard">
+            <img src={logoPurple} alt="gobarber" />
+          </Link>
+          <Link to="/dashboard">
+            <span>DASHBOARD</span>
+          </Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>Matheus Bernardi</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Matheus"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
